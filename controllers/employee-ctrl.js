1771b786d8ba76c2dc65d8b833e8fe95582a9892
Adobe-Android <tngamerboy@gmail.com>
2017-10-11 17:00:31 -0500

    got the get one employee function working

--- controllers/employee-ctrl.js
+++ controllers/employee-ctrl.js
@@ -4,9 +4,28 @@ module.exports.getEmployees = (req, res, next) => {
   const { Employees } = req.app.get('models');
   Employees.findAll()
   .then( (employees) => {
+    console.log("GETALL", employees)
     res.render('employees', {employees});
   })
   .catch( (err) => {
+    next(err);
+  });
+};
+
+module.exports.getOneEmployee = (req, res, next) => {
+  const { Employees } = req.app.get('models');
+  // console.log(req.params.id, "id");
+  Employees.findById(req.params.id)
+  .then( (data) => {
+    const {dataValues} = data;
+    let employees = [dataValues];
+    // employees = employees.push(dataValues);
+    console.log("employees", employees);
+
+    res.render('employees', {employees});
+  })
+  .catch( (err) => {
+    console.log('errrrr!')
     next(err);
   });
 };
