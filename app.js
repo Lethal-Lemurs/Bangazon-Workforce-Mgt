c76d10b55231f83f3d59125ef335e5e8bb64f3af
Glen Vollmer <glenvollmer@Glens-Macbook-Pro.local>
2017-10-12 11:54:36 -0500

    we now have the functionality to add to the database

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ let bodyParser = require('body-parser');
 require('dotenv').config();
 const port = process.env.PORT || 8080
 
+//glen got this from stackoverflow https://stackoverflow.com/questions/31435539/posting-form-data-with-nodejs-and-body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // using require('./models') to get the models may create more than one connection to the database. To avoid that, the models variable must be somehow singleton-esque. This can be achieved by attaching the models module to the application:
 app.set('models', require('./models')); //pulls in models/index.js by default. Index exports all the models you define in the models folder. So cool.
 // And when you need to require a class of the model in a controller, use this insise a middleware function rather than a direct import:
