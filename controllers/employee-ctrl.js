52321fa2922fbade316e787c533ebd3d49b80503
Glen Vollmer <glenvollmer@Glens-Macbook-Pro.local>
2017-10-13 09:10:47 -0500

    now able to add a customer to the database, still working on add the department id for employee

--- controllers/employee-ctrl.js
+++ controllers/employee-ctrl.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// const { getDepartments } = require('../controllers/department-ctrl');
+
 module.exports.getEmployees = (req, res, next) => {
   const { Employees } = req.app.get('models');
   Employees.findAll()
@@ -24,3 +26,35 @@ module.exports.getOneEmployee = (req, res, next) => {
     next(err);
   });
 };
+
+module.exports.displayNewEmployeeForm = (req, res, next) => {
+  const { Departments } = req.app.get('models');
+  Departments.findAll() 
+  .then( (departments) => {
+    res.render('create-employee', {
+      departments
+    });
+  })
+  .catch( (err) => {
+    next(err);
+  });
+};
+
+module.exports.postEmployee = (req, res, next) => {
+  // let dropdown = document.getElementById('dropdown');
+  // let dropdownValue = dropdown.options[dropdown.selectedIndex].value
+  const { Employees } = req.app.get('models');
+  Employees.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    start_date: new Date(),
+    department: "shit"
+    // computer: req.body.computer
+  })
+  .then( () => {
+    res.redirect('employees');
+  })
+  .catch( (err) => {
+    console.log(err);    
+  })
+}
--- routes/employee-route.js
+++ routes/employee-route.js
@@ -3,10 +3,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getEmployees, getOneEmployee } = require('../controllers/employee-ctrl');
+const { getEmployees, getOneEmployee, displayNewEmployeeForm, postEmployee } = require('../controllers/employee-ctrl');
 
 // When the request is a GET on the employees route, call get employees
 router.get('/employees', getEmployees);
 router.get('/employees/:id', getOneEmployee);
+router.get('/postEmployee', displayNewEmployeeForm);
+router.post('/postEmployee', postEmployee);
 
 module.exports = router;
